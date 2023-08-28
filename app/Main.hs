4ec7c7e8766a5c6fb37a15a9570d933402414812
James Butcher <jamesbutcher167@gmail.com>
2023-08-28 00:43:21 +0000

    Add viewport code

--- app/Main.hs
+++ app/Main.hs
@@ -1,29 +1,44 @@
 module Main where
 
-import GHC.Float (double2Int)
+import GHC.Float (double2Int, int2Double)
 
-type V3 = (Float, Float, Float)
-
-negateV3 :: V3 -> V3
-addV3 :: V3 -> V3 -> V3
-subV3 :: V3 -> V3 -> V3
-scaleV3 :: V3 -> Float -> V3
-
-negateV3 (x, y, z) = (-x, -y, -z)
-addV3 (x1, y1, z1) (x2, y2, z2) = (x1 + x2, y1 + y2, z1 + z2)
-subV3 v1 v2 = v1 `addV3` negateV3 v2
-scaleV3 (x, y, z) a = (x * a, y * a, z * a)
-
-type RGB = (Double, Double, Double)
-
-data PPM = PPM
-  { size :: (Int, Int),
-    content :: [RGB]
-  }
+type V3 = (Double, Double, Double)
 
 data Ray = Ray
   { dir :: V3,
     origin :: V3
+  }
+
+negateV3 :: V3 -> V3
+addV3 :: V3 -> V3 -> V3
+subV3 :: V3 -> V3 -> V3
+scaleV3 :: V3 -> Double -> V3
+negateV3 (x, y, z) = (-x, -y, -z)
+
+addV3 (x1, y1, z1) (x2, y2, z2) = (x1 + x2, y1 + y2, z1 + z2)
+
+subV3 v1 v2 = v1 `addV3` negateV3 v2
+
+scaleV3 (x, y, z) a = (x * a, y * a, z * a)
+
+rayAt :: Ray -> Double -> V3
+rayAt r a = dir r `scaleV3` a `addV3` origin r
+
+viewportRay :: V3 -> Double -> (Double, Double) -> (Double, Double) -> (Int, Int) -> Ray
+viewportRay origin@(ox, oy, oz) focalLength (tlx, tly) (dx, dy) (x, y) =
+  Ray
+    { origin = origin,
+      dir = (ox + tlx + dx * int2Double x, oy + tly - dy * int2Double y, oz + focalLength)
+    }
+
+castRay :: Ray -> RGB
+castRay _ = (0, 0, 0)
+
+type RGB = V3
+
+data PPM = PPM
+  { size :: (Int, Int),
+    content :: [RGB]
   }
 
 rgb2String :: RGB -> String
@@ -46,8 +61,16 @@ main = do
   where
     file =
       PPM
-        { size = (256, 256),
+        { size = imageSize,
           content =
-            [ (j / 256, i / 256, 0) | i <- [0 .. 255], j <- [0 .. 255]
+            [ castRay $ viewportRay origin focalLength topLeft pixelDelta (j, i) | i <- [0 .. imageWidth - 1], j <- [0 .. imageHeight - 1]
             ]
         }
+    aspectRatio = 16 / 9
+    origin@(ox, oy, oz) = (0, 0, 0) :: V3
+    imageSize@(imageWidth, imageHeight) = (800, 450) :: (Int, Int)
+    topLeft = (ox - 0.5 * viewportWidth, oy + 0.5 * viewportHeight) :: (Double, Double)
+    pixelDelta = (viewportWidth / int2Double imageWidth, viewportWidth / int2Double imageHeight) :: (Double, Double)
+    focalLength = 1 :: Double
+    viewportHeight = 2 :: Double
+    viewportWidth = viewportHeight * aspectRatio :: Double
